db9253873c8772ec218c39dd1e4a10abed88a0d8
Vinícius Martins Pedro <vinicius.martins.developer@gmail.com>
2024-03-28 10:37:12 -0300

    Finalizando o editor de texto em rust!

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,27 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+#![warn(clippy::all, clippy::pedantic, clippy::restriction)]
+#![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::implicit_return,
+    clippy::shadow_reuse,
+    clippy::print_stdout,
+    clippy::wildcard_enum_match_arm,
+    clippy::else_if_without_else
+)]
+mod document;
+mod editor;
+mod filetype;
+mod highlighting;
+mod row;
+mod terminal;
+pub use document::Document;
+use editor::Editor;
+pub use editor::Position;
+pub use editor::SearchDirection;
+pub use filetype::FileType;
+pub use filetype::HighlightingOptions;
+pub use row::Row;
+pub use terminal::Terminal;
 
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for b in io::stdin().bytes() {        
-        match b {
-            Ok(b) => {                
-                let c = b as char;
-        
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
-            }
-            Err(err) => die(err),
-
-        }
-    }
+    Editor::default().run();
 }
